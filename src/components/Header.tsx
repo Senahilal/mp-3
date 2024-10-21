de5635ad3cc6fe1ef2b14c84c34c19b94a1c2bad
Senahilal <senab@bu.edu>
2024-10-21 04:17:59 -0400

    made header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,21 @@ const StyledHeader =styled.header`
     background-color:#2b7a78;
     color:#17252a;
     align-content: left;
+
+    @media screen and (max-width: 750px) {
+        align-content: center;
+    }
 `;
 const StyledDiv =styled.div`
     max-width: 30%;
     text-align: center;
     padding: 1% 0.5%;
     font-size: 85%;
+
+    @media screen and (max-width: 750px) {
+        margin: auto;
+        max-width: 50%;
+    }
 `;
 const StyledH1 =styled.h1``;
 const StyledP =styled.p``;
